984e0e500589c4e628cb135776cefd6385fff358
Amarylliiss <wbdesignmmk@gmail.com>
2022-10-11 15:27:02 +0200

    Kalkulator Kredytowy_1

---
+++ CalcCredit/src/com/jsfcourse/calc/Credit.java
@@ -0,0 +1,90 @@
+package com.jsfcourse.calc;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class Credit {
+	private String kwota;
+	private String okres;
+	private String proc;
+	private Double result;
+
+	@Inject
+	FacesContext ctx;
+
+	
+
+	public String getKwota() {
+		return kwota;
+	}
+
+	public void setKwota(String kwota) {
+		this.kwota = kwota;
+	}
+
+	public String getOkres() {
+		return okres;
+	}
+
+	public void setOkres(String okres) {
+		this.okres = okres;
+	}
+
+	public String getProc() {
+		return proc;
+	}
+
+	public void setProc(String proc) {
+		this.proc = proc;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+
+	public boolean doTheMath() {
+		try {
+			double kwota = Double.parseDouble(this.kwota);
+			double okres = Double.parseDouble(this.okres);
+			double proc = Double.parseDouble(this.proc);
+
+			result = (((proc/100) * kwota * okres) + kwota)/(okres * 12);
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+
+	public String calc() {
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+	}
+
+
+	public String ajax() {
+		if (doTheMath()) {
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Wynik: " + result, null));
+		}
+		return null;
+	}
+
+
+}
